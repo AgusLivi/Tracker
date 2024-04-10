770a65c32eadc683eb2b6609ff41a36058ffcde2
AgustinLiviñi <agustinlivini07@gmail.com>
2024-04-10 19:58:10 -0300

    changes

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Stack, Typography} from '@mui/material'
+import Icon from '../assets/assets/icons/gym.png'
 
-function BodyPart() {
+function BodyPart( item, setBodyPart, bodyPart) {
   return (
-    <div>BodyPart</div>
+    <Stack
+      type='button'
+      alignItems='center'
+      justifyContent='center'
+      className='bodyPart-card'
+
+      sx={{  
+      borderTop:   bodyPart === item ? '4px solid #ff2625' : '' ,  
+        backgroundColor: '#fff',
+        borderBottomLeftRadius: '20px',
+        width: '270px',
+        height: '280px',
+        cursor: 'pointer',
+        gap: '47px'}}
+        
+        onCLick={() => { 
+        setBodyPart(item);
+        window.scrollTo({top: 1800, left: 100, bhavior: 'smooth'})
+        }}
+    >
+      <img src={Icon} alt='dumbell' styles={{ width: '40px', height: '40px'}}></img>
+      <Typography 
+      fontSize="24px"
+      fontWeight='bold'
+      color='#3A1212'
+      textTransform='capitalize'
+      >{item}</Typography>
+    </Stack>
   )
 }
 
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': '637ee3cf28mshd7c20c06b70f695p1e8bb7jsn3b7ed2037d31',
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
     }
   };
 
