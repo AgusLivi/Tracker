cf5783b207d64430405270d271ed8b2b93333ddf
AgustinLiviñi <agustinlivini07@gmail.com>
2024-05-15 19:52:31 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
-import { Box } from '@mui/material'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 import './App.css';
-import ExcerciseDetail from './pages/ExerciseDetail'
+import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 
-const App = () => {
-  return (
-    <Box width="400px" sx={{width: {x1: 1488}}} m='auto'>
-        <Navbar />
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/exercise/:id' element={<ExcerciseDetail />} />
-            
-        </Routes>
-    </Box>
-   
-  )
-}
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
-export default App
+export default App;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,13 +1,22 @@
 export const exerciseOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '637ee3cf28mshd7c20c06b70f695p1e8bb7jsn3b7ed2037d31',
-      'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
-    }
-  };
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '637ee3cf28mshd7c20c06b70f695p1e8bb7jsn3b7ed2037d31',
+  },
+};
 
 export const fetchData = async (url, options) => {
-     const response = await fetch(url, options)
-     const data = await response.json();
-     return data
-}
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
